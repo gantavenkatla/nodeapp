fc1e8c442d153ca233bcb593f482f098e70c0614
VenkatGanta <VenkatGanta@ML-C02SF124G8WM.farmersinsurance.com>
2018-05-07 23:58:06 -0700

    updated App root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'replace-vehicle-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
